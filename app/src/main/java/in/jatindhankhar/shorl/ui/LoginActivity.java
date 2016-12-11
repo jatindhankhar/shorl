c4e967d37f12f1e0d24fb2efc985ac8c34de8f1e
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-12-11 20:09:13 +0530

    Added Login Activity

--- app/src/main/java/in/jatindhankhar/shorl/ui/LoginActivity.java
+++ app/src/main/java/in/jatindhankhar/shorl/ui/LoginActivity.java
@@ -1,24 +1,124 @@
 package in.jatindhankhar.shorl.ui;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import in.jatindhankhar.shorl.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    public static final String PREF_FILE = "ShorlPref"; // TODO - Set name programmatically
+    public static final String IS_LOGGED_IN = "IS_LOGGED_IN";
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.sign_in_button)
+    SignInButton signInButton;
+    private GoogleApiClient mGoogleApiClient;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        mContext = getApplicationContext();
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* Activity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mContext = getApplicationContext();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 
+
+    @OnClick(R.id.sign_in_button)
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean loginSuccess) {
+        if(loginSuccess)
+        {
+            setLoginSession();
+            startActivity(new Intent(mContext,MainActivity.class));
+        }
+        else
+        {
+            clearLoginSuccess();
+        }
+    }
+
+    public void setLoginSession()
+    {
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_FILE,mContext.MODE_PRIVATE);
+        sharedPreferences.edit().putBoolean(IS_LOGGED_IN,true).apply();
+    }
+
+    public void clearLoginSuccess()
+    {
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(PREF_FILE,mContext.MODE_PRIVATE);
+        sharedPreferences.edit().putBoolean(IS_LOGGED_IN,false).apply();
+    }
 }
--- app/src/main/java/in/jatindhankhar/shorl/ui/SplashActivity.java
+++ app/src/main/java/in/jatindhankhar/shorl/ui/SplashActivity.java
@@ -1,5 +1,6 @@
 package in.jatindhankhar.shorl.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,12 +8,24 @@ import android.os.Bundle;
 
 
 public class SplashActivity extends AppCompatActivity {
-
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this,LoginActivity.class));
+        mContext = getApplicationContext();
+
+        // Launch correct acitivity bases on the user session state
+        if(isLoggedIn())
+            startActivity(new Intent(this,LoginActivity.class));
+        else
+        startActivity(new Intent(this,MainActivity.class));
+
         finish();
 
+    }
+
+    private boolean isLoggedIn()
+    {
+        return mContext.getSharedPreferences(LoginActivity.PREF_FILE,mContext.MODE_PRIVATE).getBoolean(LoginActivity.IS_LOGGED_IN,false);
     }
 }
