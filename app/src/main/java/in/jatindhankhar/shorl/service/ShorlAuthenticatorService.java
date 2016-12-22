d87399742785d6fb564a08bdb5f554437a0d16c1
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-12-22 21:36:49 +0530

    Register the authenticator service

---
+++ app/src/main/java/in/jatindhankhar/shorl/service/ShorlAuthenticatorService.java
@@ -0,0 +1,21 @@
+package in.jatindhankhar.shorl.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import in.jatindhankhar.shorl.ui.ShorlAuthenticator;
+
+/**
+ * Created by jatin on 12/22/16.
+ */
+
+public class ShorlAuthenticatorService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        ShorlAuthenticator shorlAuthenticator = new ShorlAuthenticator(this);
+        return  shorlAuthenticator.getIBinder();
+    }
+}
