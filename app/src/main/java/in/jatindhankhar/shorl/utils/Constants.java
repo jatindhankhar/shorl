59ace84c30c0e714afe4de6b6f493562919cd44b
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-12-27 21:31:12 +0530

    Added new fields and helper functions

--- app/src/main/java/in/jatindhankhar/shorl/utils/Constants.java
+++ app/src/main/java/in/jatindhankhar/shorl/utils/Constants.java
@@ -11,6 +11,7 @@ public final  class Constants {
     public static final String PREF_FILE = "ShorlPref"; // TODO - Set name programmatically
     public static final String IS_LOGGED_IN = "IS_LOGGED_IN";
     public static final String ACCOUNT_NAME = "ACCOUNT_NAME";
+    public static final String ACCOUNT_EMAIL = "ACCOUNT_EMAIL";
     public static final String URL_SHORTNER_SCOPE = "oauth2: https://www.googleapis.com/auth/urlshortener";
     public final static String ARG_ACCOUNT_TYPE = "ACCOUNT_TYPE";
     public final static String ARG_AUTH_TYPE = "AUTH_TYPE";
--- app/src/main/java/in/jatindhankhar/shorl/utils/Utils.java
+++ app/src/main/java/in/jatindhankhar/shorl/utils/Utils.java
@@ -2,7 +2,10 @@ package in.jatindhankhar.shorl.utils;
 
 import android.accounts.AccountManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+
+import in.jatindhankhar.shorl.ui.LoginActivity;
 
 /**
  * Created by jatin on 12/23/16.
@@ -20,24 +23,39 @@ public final class Utils {
     {
         SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.PREF_FILE,context.MODE_PRIVATE);
         sharedPreferences.edit().putBoolean(Constants.IS_LOGGED_IN,false).apply();
+        sharedPreferences.edit().remove(Constants.ACCOUNT_NAME).apply();
+        sharedPreferences.edit().remove(Constants.AUTH_TOKEN).apply();
+        sharedPreferences.edit().remove(Constants.ACCOUNT_EMAIL).apply();
     }
 
-    public static void setLoginSession(Context context,String accountName)
+    public static void setLoginSession(Context context,String accountName,String accountEmail)
     {
         SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.PREF_FILE,context.MODE_PRIVATE);
-        sharedPreferences.edit().putString(Constants.ACCOUNT_NAME,accountName).apply();
-        sharedPreferences.edit().putBoolean(Constants.IS_LOGGED_IN,true).apply();
+        sharedPreferences.edit().putString(Constants.ACCOUNT_NAME,accountName).
+                putBoolean(Constants.IS_LOGGED_IN,true).
+                putString(Constants.ACCOUNT_EMAIL,accountEmail).apply();
+    }
+
+    public static void setAuthToken(Context context,String authToken)
+    {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.PREF_FILE,context.MODE_PRIVATE);
+        sharedPreferences.edit().putString(Constants.AUTH_TOKEN,authToken).apply();
     }
 
     public static String getLoginName(Context context)
     {
-        return context.getSharedPreferences(Constants.PREF_FILE,context.MODE_PRIVATE).getString(Constants.ACCOUNT_NAME," ");
+        return context.getSharedPreferences(Constants.PREF_FILE,context.MODE_PRIVATE).getString(Constants.ACCOUNT_NAME,"");
+    }
+
+    public static String getLoginEmail(Context context)
+    {
+        return  context.getSharedPreferences(Constants.PREF_FILE,context.MODE_PRIVATE).getString(Constants.ACCOUNT_EMAIL,"");
     }
 
     public static String getAuthToken(Context context)
     {
-       // AccountManager.get(context).
-
-        return null;
+        return context.getSharedPreferences(Constants.PREF_FILE,context.MODE_PRIVATE).getString(Constants.AUTH_TOKEN,"");
     }
+
+
 }
