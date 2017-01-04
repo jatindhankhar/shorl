019e94d6a31730e62e5e74900771d169fa0cdf85
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-01-04 22:09:02 +0530

    Added new menu item and library

---
+++ app/src/main/java/in/jatindhankhar/shorl/ui/custom/FABScrollBehavior.java
@@ -0,0 +1,10 @@
+package in.jatindhankhar.shorl.ui.custom;
+
+import android.support.design.widget.FloatingActionButton;
+
+/**
+ * Created by jatin on 1/4/17.
+ */
+
+public class FABScrollBehavior extends FloatingActionButton.Behavior {
+}
