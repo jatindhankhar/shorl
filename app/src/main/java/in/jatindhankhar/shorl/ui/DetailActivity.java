2a840094d35424a9d941a8921ad35d71b0f6c437
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-01-10 23:42:34 +0530

    Add detail activity layout with qr codes :smile:

--- app/src/main/java/in/jatindhankhar/shorl/ui/DetailActivity.java
+++ app/src/main/java/in/jatindhankhar/shorl/ui/DetailActivity.java
@@ -1,10 +1,15 @@
 package in.jatindhankhar.shorl.ui;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+
+import net.glxn.qrgen.android.QRCode;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +18,8 @@ import in.jatindhankhar.shorl.R;
 public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.qrcode)
+    ImageView qrcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +31,8 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Detail");
         Toast.makeText(this, "Yo " + getIntent().getStringExtra("test"), Toast.LENGTH_SHORT).show();
+        qrcode.setImageBitmap(QRCode.from("www.example.org").bitmap());
+
 
     }
 
