22a8a676dd65f80348e928945c4a654633cb8932
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-01-10 15:44:37 +0530

    Add minimal working widget code. Need to expand and iterate over it

---
+++ app/src/main/java/in/jatindhankhar/shorl/widget/WidgetProvider.java
@@ -0,0 +1,42 @@
+package in.jatindhankhar.shorl.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.widget.RemoteViews;
+
+import in.jatindhankhar.shorl.R;
+import in.jatindhankhar.shorl.ui.MainActivity;
+
+/**
+ * Created by jatin on 1/9/17.
+ */
+
+public class WidgetProvider extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
+}
